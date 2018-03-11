df49abe11e64c13aeae6e919fb7b0b7e65495e7b
wz337 <31293777+wz337@users.noreply.github.com>
2018-03-11 16:26:28 -0400

    Update 90.SubsetsII.java

--- 90.SubsetsII.java
+++ 90.SubsetsII.java
@@ -1,31 +1,42 @@
-//Time: O(2^n)
-//Space: O(n)
+//do we return all unique combination?
+//Time: O(2^n), Space: O(n)
+//Key point: Only one copy of duplicates can show up under same parents.
+//
+/*           ""
+          /      \
+a       a        {}
+b      / \       / \
+     ab   |
+    /\ \  |
+b abb | | |
+          a0bc
+        a1bc
+    a2bc
+*/
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         if (nums == null || nums.length < 1) {
             return result;
         }
-        Arrays.sort(nums); //Remember to sort the array to dedup.
+        //Make sure the array is sorted so we can dedup
+        Arrays.sort(nums);  //remember to sort the array please...how else do you know if there are duplicates
         List<Integer> cur = new ArrayList<>();
-        dfs(nums, 0, cur, result);
+        helper(0, nums, cur, result);
         return result;
     }
-    private void dfs(int[] nums, int index, List<Integer> cur, List<List<Integer>> result) {
-        if (index == nums.length) {
-            result.add(new ArrayList(cur));
+    private void helper(int index, int[] nums, List<Integer> cur, List<List<Integer>> result) {
+        if (nums.length == index) {
+            result.add(new ArrayList<Integer>(cur));
             return;
         }
         cur.add(nums[index]);
-        dfs(nums, index + 1, cur, result);
+        helper(index + 1, nums, cur, result);
         cur.remove(cur.size() - 1);
-        while (index + 1 < nums.length && nums[index] == nums[index + 1]) {
+        //skip all the consecutive and duplicates number
+        while (index + 1 < nums.length && nums[index + 1] == nums[index]) {
             index++;
         }
-        dfs(nums, index + 1, cur, result);
+        helper(index + 1, nums, cur, result);
     }
 }
-
-
-
-
