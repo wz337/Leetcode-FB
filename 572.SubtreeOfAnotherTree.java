879c57d087746319b6e9a670e57835d2c68ea3c5
wz337 <31293777+wz337@users.noreply.github.com>
2018-03-11 15:49:11 -0400

    Create 572.SubtreeOfAnotherTree.java

---
+++ 572.SubtreeOfAnotherTree.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+//Time: O(m + n) m: the number of nodes in s, n: the number of nodes in t
+//Space: O(m) worst case, if s is a linkedlist. 
+
+class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        } else if (s == null || t == null) {
+            return false;
+        }
+        if (isSameTree(s, t)) {
+            return true;
+        }
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+    private boolean isSameTree(TreeNode one, TreeNode two) {
+        if (one == null && two == null) {
+            return true;
+        } else if (one == null || two == null) {
+            return false;
+        } else if (one.val != two.val) {
+            return false;
+        }
+        return isSameTree(one.left, two.left) && isSameTree(one.right, two.right);
+    }
+}
