646c78e55a267a58742f47d1ff66cfc66e4617a3
wz337 <31293777+wz337@users.noreply.github.com>
2018-03-12 00:08:39 -0400

    Create 49.GroupAnagrams.java

---
+++ 49.GroupAnagrams.java
@@ -0,0 +1,29 @@
+//The key of this problem is to find a universal representation for each anagram group.
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        if (strs == null || strs.length < 1) {
+            return result;
+        }
+        //Key: count of the letters in the word, value: list of anagram that matches the key
+        Map<String, List<String>> map = new HashMap<>();
+        int[] count = new int[26];
+        for (String s : strs) {
+            Arrays.fill(count, 0);
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < count.length; i++) {
+                sb.append("#");
+                sb.append(count[i]);
+            }
+            String key = sb.toString();
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+        return new ArrayList(map.values());
+    }
+}
